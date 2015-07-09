4af922426a2fc81c112548a6cf6bf9e5e356c451
leopoldkwok <leopoldkwok@gmail.com>
2015-07-09 19:04:33 +0100

    Introduction to Python

---
+++ 02_djangogirls_tutorial/djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/leopoldkwok/Documents/practise_code/02_djangogirls_tutorial/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ 02_djangogirls_tutorial/djangogirls/python_intro.py
@@ -0,0 +1,16 @@
+if 3 > 2:
+    print('It works!')
+
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+# print('Hello, Django girls!')
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
