5d0155d45aa96db33968fe0fb462128874dec15f
leopoldkwok <leopoldkwok@gmail.com>
2015-09-22 13:33:08 +0100

    The Scope Service

--- 05_learn_and_understand_angularJS/app.js
+++ 05_learn_and_understand_angularJS/app.js
@@ -2,8 +2,15 @@
 var myApp = angular.module('myApp', []);
 
 // CONTROLLERS
-myApp.controller('mainController', function() {
-
-
-
+myApp.controller('mainController', function ($scope) {
+    
+    $scope.name = 'Jane Doe';
+    $scope.occupation = 'Coder';
+    
+    $scope.getname=function() {
+        return 'John Doe';
+    }
+    
+//    $scope.getname();
+    console.log($scope);
 });
