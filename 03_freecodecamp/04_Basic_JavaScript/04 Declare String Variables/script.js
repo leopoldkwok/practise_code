9a136be603a1b4953349471b61372587507682c1
leopoldkwok <leopoldkwok@gmail.com>
2015-10-17 23:11:37 +0100

    Declare String Variables

---
+++ 03_freecodecamp/04_Basic_JavaScript/04 Declare String Variables/script.js
@@ -0,0 +1,12 @@
+// var name = "Alan Turing";
+// var firstName = "Alan";
+// var lastName = "Turing";
+
+var myFirstName = "Leopold";
+var myLastName = "Kwok";
+
+
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+if(typeof(myFirstName) !== "undefined" && typeof(myLastName) !== "undefined"){(function(){return myFirstName + ', ' + myLastName;})();}
