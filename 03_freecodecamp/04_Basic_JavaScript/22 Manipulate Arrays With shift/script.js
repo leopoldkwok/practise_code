14fbb12e8b5083496291d92aea4c27fef56600cb
leopoldkwok <leopoldkwok@gmail.com>
2015-10-19 15:34:00 +0100

    Manipulate Arrays With shift

---
+++ 03_freecodecamp/04_Basic_JavaScript/22 Manipulate Arrays With shift/script.js
@@ -0,0 +1,15 @@
+var ourArray = ["Stimpson", "J", ["cat"]];
+ourRemoved = ourArray.shift();
+// ourArray now equals ["J", ["cat"]].
+
+var myArray = ["John", 23, ["dog", 3]];
+// Only change code below this line.
+
+var myRemoved = myArray; // This should be ["John"] and myArray should now be [23, ["dog", 3]].
+
+// Only change code above this line.
+
+var myRemoved =  myArray.shift();
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(y, z){return 'myArray = ' + JSON.stringify(y) + ' & myRemoved = ' + JSON.stringify(z);})(myArray, myRemoved);
