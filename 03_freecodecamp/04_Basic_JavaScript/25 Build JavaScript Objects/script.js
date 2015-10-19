94f76d269dfe623b5c22e7a080b030b2a891ba75
leopoldkwok <leopoldkwok@gmail.com>
2015-10-19 15:50:09 +0100

    Build JavaScript Objects

---
+++ 03_freecodecamp/04_Basic_JavaScript/25 Build JavaScript Objects/script.js
@@ -0,0 +1,24 @@
+// var ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"]
+// };
+
+// Add the name (string), legs (number), tails (number) and friends (array) properties to myDog.
+// You can set them to whatever you want.
+
+// Only change code below this line.
+
+var myDog = {
+  
+  "name": "Sophie",
+  "legs": 4,
+  "tails": 1,
+  "friends":["Bono", "Janice","Leo"]
+   
+};
+
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+(function(z){return z;})(myDog);
