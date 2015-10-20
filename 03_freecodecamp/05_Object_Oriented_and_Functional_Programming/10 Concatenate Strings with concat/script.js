00c7e1cf40060fcbecaa0fa9b4ba14c4bad6fd32
leopoldkwok <leopoldkwok@gmail.com>
2015-10-20 15:29:14 +0100

    Concatenate Strings with concat

---
+++ 03_freecodecamp/05_Object_Oriented_and_Functional_Programming/10 Concatenate Strings with concat/script.js
@@ -0,0 +1,9 @@
+var array = [1,2,3];
+
+var concatMe = [4,5,6];
+// Only change code below this line.
+
+var array = array.concat(concatMe);
+
+// Only change code above this line.
+(function() {return array;})();
