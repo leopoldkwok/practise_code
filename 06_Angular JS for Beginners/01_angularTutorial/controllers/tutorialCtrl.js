d315228ef74dbf28041e1bef536f9f6f2377387c
leopoldkwok <leopoldkwok@gmail.com>
2015-10-21 18:36:27 +0100

    Expressions And Binding

--- 06_Angular JS for Beginners/01_angularTutorial/controllers/tutorialCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module("tutorialCtrlModule",[])
-
-.controller("TutorialCtrl",["$scope", function($scope) {
-
-
-	$scope.name = "thomas";
-	// our programing work is done
-}]);
---
+++ 06_Angular JS for Beginners/01_angularTutorial/js/controllers/tutorialCtrl.js
@@ -0,0 +1,16 @@
+angular.module("tutorialCtrlModule",[])
+
+.controller("TutorialCtrl",["$scope", function($scope) {
+
+	$scope.tutorialObject = {};
+	$scope.tutorialObject.title = "Main Page";
+	$scope.tutorialObject.subTitle = "Sub title";
+
+	$scope.tutorialObject.bindOutput = 2;
+
+	$scope.timesTwo = function() {
+		$scope.tutorialObject.bindOutput *=2;
+	}
+	
+	// our programing work is done
+}]);
