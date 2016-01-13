516b38d86f41054bd6cc3b20ca88ea2e9aa460a2
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-13 11:44:25 +0000

    giving descriptions to grunt tasks

--- 10_using_grunt/05_kicking_tires/Gruntfile.js
+++ 10_using_grunt/05_kicking_tires/Gruntfile.js
@@ -8,12 +8,29 @@ module.exports = function(grunt) {
 		grunt.log.writeln('Hi there, ' + name);
 	});
 
+	// using two arguements
 	grunt.registerTask('addNumbers', function(first, second) {
 		if(isNaN(Number(first))) {
 			grunt.warn('The first argument must be a number');
 		}
 		var answer = Number(first) + Number(second);
 		grunt.log.writeln(first + ' + ' + second + ' is ' + answer);
+	});
+
+	//chanining tasks
+	grunt.registerTask('all', ['default', 'greet:Brian', 'addNumbers:2:3']);
+
+	grunt.registerTask('praise',
+					   'Have Grunt say nice things about you', function() {
+
+		var praise = [
+			"You're awesome.",
+			"You're the best developer ever!",
+			"You are extremely attractive.",
+			"Everyone loves you!"
+			]
+		var pick = praise[(Math.floor(Math.random() * praise.length))];
+		grunt.log.writeln(pick);
 	});
 
 }
