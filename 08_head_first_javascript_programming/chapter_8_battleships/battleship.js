e68c97fdcecdf60f46514c4630cff1e53e234fce
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-20 11:04:48 +0000

    Implementing the View

---
+++ 08_head_first_javascript_programming/chapter_8_battleships/battleship.js
@@ -0,0 +1,15 @@
+// defining an object and assigning it to the variable view.
+var view = {
+	// this method takes a string message and displays it
+	// in the message display area
+	displayMessage: function(msg) {
+
+	},
+	displayHit: function(location) {
+
+	},
+	displayMiss: function(location) {
+
+	}
+
+};
