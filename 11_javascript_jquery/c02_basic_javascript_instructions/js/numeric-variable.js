f5e2ac3fb3e4e87498a3a6a1cea5b5f410f6050f
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-24 20:49:49 +0000

    numeric variables

---
+++ 11_javascript_jquery/c02_basic_javascript_instructions/js/numeric-variable.js
@@ -0,0 +1,22 @@
+// Create three variables to store the information needed
+
+var price
+var quantity;
+var total
+
+// Assign values to the price and quantity variables
+price = 5;
+quantity = 200;
+
+// calculate the total by multiplying the price by quantity
+total = price * quantity;
+
+//get the element with an id of cost
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
+
+/*
+NOTE: textContent does not work in IE8 or earlier
+You can use innerHTML, but note the security issues
+el.innerHTML = '$' + total;
+*/
