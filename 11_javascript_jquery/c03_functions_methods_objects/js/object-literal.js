0885e229727b7addd0690da0ac7b6501201cf36d
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-25 13:25:22 +0000

    Object literal

---
+++ 11_javascript_jquery/c03_functions_methods_objects/js/object-literal.js
@@ -0,0 +1,17 @@
+// Set up the object
+var hotel = {
+	name: 'Quay',
+	rooms: 100,
+	booked: 25,
+	checkAvailability: function() {
+		return this.rooms - this.booked; // Need "this" because inside function
+	}
+};
+
+// Update the HTML
+var elName = document.getElementById('hotelName'); // Get Element
+elName.textContent = hotel.name; // Update HTML with property of the object
+
+var elRooms = document.getElementById('rooms');	// Get element
+elRooms.textContent = hotel.checkAvailability(); // Update HTML with property of the object
+
