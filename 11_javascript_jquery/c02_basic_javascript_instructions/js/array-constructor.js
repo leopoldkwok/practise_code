5189437d8faca545a5e71edb92863c18a5193217
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-25 09:54:40 +0000

    array constructor

---
+++ 11_javascript_jquery/c02_basic_javascript_instructions/js/array-constructor.js
@@ -0,0 +1,9 @@
+// Create and name the variable.
+// Tell the interpreter it is an array
+// Assign values nside the parentheses.
+
+var colors = new Array('white', 'black', 'custom');
+
+// Show the first item from the array
+var el = document.getElementById('colors');
+el.textContent = colors[2];
