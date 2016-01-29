46f5a21b0a825413f0fecfe0d17fa8ae58002d68
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-29 00:19:41 +0000

    if else statement

---
+++ 11_javascript_jquery/c04_decisions_and_loops/js/if-else-statement.js
@@ -0,0 +1,13 @@
+var pass = 50; 		// Pass mark
+var score = 75;		// Current Score
+var msg;			// Message
+
+// Select message to write based on score
+if (score > pass) {
+	msg = "congratulations, you passed!";
+} else {
+	msg = 'Have another go!';
+}
+
+var el = document.getElementById('answer');
+el.textContent = msg;
