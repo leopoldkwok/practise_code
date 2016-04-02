bf2554da7c835d3a423b36d7c6f9a534329cfddd
“leopoldkwok” <leopoldkwok@gmail.com>
2016-04-02 11:56:31 +0100

    add animal file

--- 12_head_first_ruby/chapter_3_inheritance/animal.rb
+++ 12_head_first_ruby/chapter_3_inheritance/animal.rb
@@ -1,4 +1,4 @@
-class Dog
+class Animal
 
 	#Creates methods to get current values of @name and @age
 	attr_reader :name, :age
@@ -33,5 +33,72 @@ class Dog
 		puts "#{@name} is #{@age} years old."
 	end
 
+end
+
+class Dog < Animal
+
+	def to_s
+		"#{@name} the dog, age #{age}" # This return value is the format we'd like to see
+	end
+end
+
+class Bird < Animal
+
+	def talk # overrides the inherited method
+		puts "#{@name} says Chirp! Chirp!"
+	end
 
 end
+
+class Cat < Animal
+
+	def talk # overrides the inherited method
+		puts "#{@name} says Meow!"
+	end
+
+end
+
+class Armadillo < Animal
+
+	def move(destination) # Overrides the "move" method from the superclass
+		puts "#{@name} unrolls!" # The new functionality
+		# super(destination) # Explicitly specify the arguement
+		super #autoforward the same argument(s) "move" was called with
+	end
+end
+
+lucy = Dog.new
+lucy.name = "Lucy"
+lucy.age = 4
+
+rex = Dog.new
+rex.name = "Rex"
+rex.age = 2
+
+puts lucy, rex
+
+puts lucy.to_s, rex.to_s
+
+# puts Dog.superclass
+# puts Animal.superclass
+
+
+
+# puts rex.methods
+
+dillon = Armadillo.new
+dillon.name = "Dillon"
+dillon.move("burrow")
+
+whiskers = Cat.new
+whiskers.name = "Whiskers"
+fido = Dog.new
+fido.name = "fido"
+polly = Bird.new
+polly.name = "Polly"
+
+polly.age = 2
+polly.report_age
+fido.move("yard")
+whiskers.talk
+polly.talk
