574988b62f51febf3d169ee5d794235b74c2961a
“leopoldkwok” <leopoldkwok@gmail.com>
2016-04-03 12:19:59 +0100

    finished candidate code

--- 12_head_first_ruby/chapter_7_hashes/candidate.rb
+++ 12_head_first_ruby/chapter_7_hashes/candidate.rb
@@ -1,7 +1,8 @@
 class Candidate
 	attr_accessor :name, :age, :occupation, :hobby, :birthplace # Set up attribute accessors
 
-	def initialize(name, age, occupation, hobby = nil, birthplace) # Set up Candiate.new to take arguments
+	# we replace the hash parameter with keywords and default values
+	def initialize(name, age:, occupation: , hobby: nil, birthplace: "Sleepy Creek")
 		# use the parameter to set the object attributes
 		self.name = name
 		self.age = age
@@ -19,5 +20,38 @@ def print_summary(candidate)
 	puts "Birthplace: #{candidate.birthplace}"
 end
 
-candidate = Candidate.new("Carl Barnes", 49, "Attorney", nil, "Miami")
-print_summary(candidate)
+p Candidate.new("Amy Nguyen")
+
+
+
+# Candidate.new("Amy Nguyen", age: 37)
+
+# p Candidate.new("Amy Nguyen")
+# p Candidate.new("Carl Barnes")
+# p Candidate.new("Carl Barnes", occupasastion: "Attorney")
+
+
+# candidate1 = Candidate.new("Carl Barnes", age: 49, occupation: "Attorney")
+# candidate2 = Candidate.new("Amy Nguyen", age: 37, occupation: "Engineer")
+
+# p candidate1, candidate2
+
+# If a hash uses symbols as keys, hash literals let you leave the colon(:) if the symbol and replace the hash rocket (=>) with a colon
+# candidate = Candidate.new("Amy Nguyen", age: 37, occupation: "Engineer", hobby: "Lacrosse")
+
+
+# p candidate
+
+# Ruby lets you leave the curly braces off as long as the hash argumenet is the final argument
+# candidate = Candidate.new("Carl Barnes", :age => 49, :occupation => "Attorney", :birthplace => "Miami")
+
+
+
+# candidate = Candidate.new("Amy Nguyen", {:age => 37, :occupation => "Engineer", :hobby => "Lacrosse", :birthplace => "Seattle"})
+
+# candidate = Candidate.new("Carl Barnes", {:age => 49, :occupation => "Attorney", :birthplace => "Miami"}) #omitted attributes default to nil
+
+
+
+# candidate = Candidate.new("Carl Barnes", 49, "Attorney", nil, "Miami")
+# print_summary(candidate)
