89d286aadedcc2d3107a950383ec955b7615bb9f
“leopoldkwok” <leopoldkwok@gmail.com>
2016-04-04 23:15:49 +0100

    Basic syntax

--- 14_Lynda-RSpec_Testing Framework_with_Ruby/car_project/spec/car_spec.rb
+++ 14_Lynda-RSpec_Testing Framework_with_Ruby/car_project/spec/car_spec.rb
@@ -1,1 +1,14 @@
 require 'car'
+
+describe 'Car' do
+
+	describe '.colors' do
+
+		it "returns an array of color names" do
+			c = ['blue', 'black', 'red', 'green']
+			expect(Car.colors).to match_array(c)
+		end
+	end
+
+
+end
