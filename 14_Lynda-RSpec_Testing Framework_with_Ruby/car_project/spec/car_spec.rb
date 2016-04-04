2c6f77a0f258a127747f3b71a337a9d57ad1346b
“leopoldkwok” <leopoldkwok@gmail.com>
2016-04-04 23:57:36 +0100

    014 Skipping examples and working with pending examples

--- 14_Lynda-RSpec_Testing Framework_with_Ruby/car_project/spec/car_spec.rb
+++ 14_Lynda-RSpec_Testing Framework_with_Ruby/car_project/spec/car_spec.rb
@@ -5,6 +5,7 @@ describe 'Car' do
 	describe 'attributes' do
 
 		it 'allows reading and writing for :make' do
+			# skip("Debugging a problem")
 			car = Car.new
 			car.make = 'Test'
 			expect(car.make).to eq('Test')
