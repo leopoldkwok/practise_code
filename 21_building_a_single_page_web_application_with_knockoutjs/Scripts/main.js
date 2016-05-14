b3fd86e25144cef0cc5239da1a6b753ca6a864bc
“leopoldkwok” <leopoldkwok@gmail.com>
2016-05-14 23:18:48 +0100

    keeping a list of documents

--- 21_building_a_single_page_web_application_with_knockoutjs/Scripts/main.js
+++ 21_building_a_single_page_web_application_with_knockoutjs/Scripts/main.js
@@ -1,10 +1,20 @@
 //Jquery on load
 $(function() {
   // TODO: Add code here
-  var doc = {
-    title: ko.observable("Hello World"),
-    body: ko.observable("Welcome to the world, my friends")
+  var doc1 = {
+    title: ko.observable("Hello"),
+    body: ko.observable("World")
   };
-  ko.applyBindings(doc);
+  var doc2 = {
+    title: ko.observable("Goodbye"),
+    body: ko.observable("World")
+  };
+  var viewModel = {
+    documents: ko.observableArray()
+  };
+  viewModel.documents.push(doc1);
+  viewModel.documents.push(doc2);
+
+  ko.applyBindings(viewModel);
 
 });
