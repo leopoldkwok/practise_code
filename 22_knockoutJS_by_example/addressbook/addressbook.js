a9ff39eafe7bf6ad3fa67cfe2a35d9339d760119
“leopoldkwok” <leopoldkwok@gmail.com>
2016-05-15 10:57:56 +0100

    Capturing and storing contacts

---
+++ 22_knockoutJS_by_example/addressbook/addressbook.js
@@ -0,0 +1,37 @@
+/* Module for Address Book application */
+var AddressBook = function () {
+
+  /* add members here */
+  var contact = {
+  name: ko.observable(),
+  phoneNumber: ko.observable()
+};
+
+var contacts = ko.observableArray();
+
+var addContact = function () {
+console.log("Adding new contact with name: " + contact.name() +" and phone number: " + contact.phoneNumber());
+
+//add the contact to the contacts array
+contacts.push({name: contact.name(), phoneNumber: contact.phoneNumber()});
+};
+
+var clearContact = function () {
+  contact.name(null);
+  contact.phoneNumber(null);
+};
+
+  var init = function () {
+  /* add code to initialize this module */
+  ko.applyBindings(AddressBook);
+};
+
+  /* execute the init function when the DOM is ready */
+  $(init);
+
+  return {
+  /* add members that will be exposed publicly */
+  contact: contact,
+  addContact: addContact
+};
+}();
