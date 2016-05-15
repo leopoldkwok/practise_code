ba8543ec45ceec68db4579155905c2ac115e7279
“leopoldkwok” <leopoldkwok@gmail.com>
2016-05-15 18:07:06 +0100

    add tasks

--- 22_knockoutJS_by_example/02_ToDoList/javascript/todolist.js
+++ 22_knockoutJS_by_example/02_ToDoList/javascript/todolist.js
@@ -2,9 +2,33 @@
 var ToDoList = function () {
 
   /* add members here */
+  /* the task */
+  var task = {
+    name: ko.observable(),
+    description: ko.observable()
+  };
+
+
+  /* array of tasks */
+  var tasks = ko.observableArray();
+
+  var addTask = function () {
+  console.log("Adding new task with name: " + task.name() +" and task description: " + task.description());
+
+  //add the contact to the contacts array
+  tasks.push({name: task.name(), description: task.description()});
+  clearTask();
+  };
+
+  /* method to clear the task */
+  var clearTask = function() {
+    task.name(null);
+    task.description(null);
+  };
 
   var init = function () {
   /* add code to initialize this module */
+  ko.applyBindings(ToDoList);
 };
 
   /* execute the init function when the DOM is ready */
@@ -12,6 +36,10 @@ var ToDoList = function () {
 
   return {
   /* add members that will be exposed publicly */
+  tasks: tasks,
+  task: task,
+  addTask: addTask
+
 
   };
 }();
