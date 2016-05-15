5121fb19591bc01767803f9754c27b783e5a4636
“leopoldkwok” <leopoldkwok@gmail.com>
2016-05-15 17:48:05 +0100

    creating the skeleton

---
+++ 22_knockoutJS_by_example/02_ToDoList/javascript/todolist.js
@@ -0,0 +1,17 @@
+/* Module for Address Book application */
+var ToDoList = function () {
+
+  /* add members here */
+
+  var init = function () {
+  /* add code to initialize this module */
+};
+
+  /* execute the init function when the DOM is ready */
+  $(init);
+
+  return {
+  /* add members that will be exposed publicly */
+
+  };
+}();
