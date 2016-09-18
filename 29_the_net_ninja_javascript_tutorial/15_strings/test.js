bd88e0c6074abdc038a834d9e7d2bd6c3029aa9d
leopoldkwok <leopoldkwok@gmail.com>
2016-09-18 20:29:42 +0100

    strings

---
+++ 29_the_net_ninja_javascript_tutorial/15_strings/test.js
@@ -0,0 +1,28 @@
+var string1 = "Abc";
+var string2 = "Bcd";
+
+console.log(string1 < string2);
+
+//console.log(string1.toLowerCase() === string2.toLowerCase());
+
+// var myString = 'I\'m a "fun ninja" string';
+
+
+//console.log(myString);
+//
+//console.log(myString.length);
+//
+//console.log(myString.toUpperCase());
+//
+//console.log(myString.indexOf("ninja"));
+
+//if (myString.indexOf("ninja") === -1 ) {
+//    
+//    console.log("the word ninja is not in the string");
+//    
+//} else {
+//    
+//    console.log("the word ninja starts at position " + myString.indexOf("ninja"));
+//    
+//}
+
