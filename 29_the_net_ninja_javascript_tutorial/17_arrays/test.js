1de8d2994c8a99c66834ac37c24f543df35fd76a
leopoldkwok <leopoldkwok@gmail.com>
2016-09-20 22:00:38 +0100

    arrays

---
+++ 29_the_net_ninja_javascript_tutorial/17_arrays/test.js
@@ -0,0 +1,32 @@
+var myArray = [];
+
+myArray[0] = 25
+
+myArray[1] = 35
+
+myArray[2] = true
+
+myArray[3]="hello"
+
+console.log(myArray);
+
+myArray[2] = false
+
+myArray
+[25, 35, false, "hello"]
+var myArray2 = [10,20,"hi", false];
+undefined
+myArray2
+[10, 20, "hi", false]
+var myArray3 = new Array()
+undefined
+var myArray4 = newArray(5)
+
+var myArray4 = new Array(5)
+undefined
+myArray2.length
+4
+myArray2.sort()
+[10, 20, false, "hi"]
+myArray2.reverse()
+["hi", false, 20, 10]
