d4643b434341363de7fe4d93baa2f898ebb2189e
leopoldkwok <leopoldkwok@gmail.com>
2016-09-22 17:45:59 +0100

    changing element attributes

---
+++ 29_the_net_ninja_javascript_tutorial/24_Changing_Element Attributes/test.js
@@ -0,0 +1,19 @@
+var link = document.getElementById("test");
+
+ink
+<a href=​"/​Subjects" id=​"test" class=​"ninja">​Subjects​</a>​
+link.getAttribute("href");
+"/Subjects"
+link.getAttribute("class");
+"ninja"
+link.setAttribute("class", "pie");
+
+link.setAttribute("alt", "hello");
+
+link.className;
+"pie"
+link.className = "ninja";
+"ninja"
+link.href;
+"http://thenetninja.co.uk/Subjects"
+link.style
