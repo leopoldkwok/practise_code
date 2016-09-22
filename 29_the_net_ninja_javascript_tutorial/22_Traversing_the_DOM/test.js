71123a099154bd99b8d22156edcd0bb491f16a69
leopoldkwok <leopoldkwok@gmail.com>
2016-09-22 16:46:07 +0100

    traversing the dom

---
+++ 29_the_net_ninja_javascript_tutorial/22_Traversing_the_DOM/test.js
@@ -0,0 +1,15 @@
+document.getElementsByClassName("content");
+
+var myContentDivs = document.getElementsByClassName("content");
+
+myContentDivs
+
+var myH2 = myContentDivs[1.getElementsByTagName("h2");
+
+var myH2 = myContentDivs[1].getElementsByTagName("h2");
+
+myH2
+
+myH2[0].innerHTML = "Yo NINJAS";
+
+document.getElementById("page-title");
