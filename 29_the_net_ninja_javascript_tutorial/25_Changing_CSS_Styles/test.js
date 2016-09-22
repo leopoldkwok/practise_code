539592028626ef1bba608dd3a564dfb9a70f5995
leopoldkwok <leopoldkwok@gmail.com>
2016-09-22 17:54:51 +0100

    changing CSS styles

---
+++ 29_the_net_ninja_javascript_tutorial/25_Changing_CSS_Styles/test.js
@@ -0,0 +1,15 @@
+var title = document.getElementById("page-title");
+
+title.setAttribute("style", "position: relative;");
+
+title.setAttribute("style", "left: 10px;");
+
+title.setAttribute("style", "position: relative; left: 10px;");
+
+title.style.left = "20px";
+
+title.style.top = "10px";
+
+title.style.color = "red";
+
+title.style.backgroundColor = "blue";
