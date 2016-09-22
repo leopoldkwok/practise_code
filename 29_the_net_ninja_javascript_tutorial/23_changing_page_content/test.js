de01412668c9d7c82fc74c500e2269b32d0143a0
leopoldkwok <leopoldkwok@gmail.com>
2016-09-22 17:21:33 +0100

    changing page content

---
+++ 29_the_net_ninja_javascript_tutorial/23_changing_page_content/test.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName("content");
+
+var myContentDivs = document.getElementsByClassName("content");
+
+myContentDivs
+
+var myH2 = myContentDivs[1.getElementsByTagName("h2");
+
+var myH2 = myContentDivs[1].getElementsByTagName("h2");
+
+myH2
+
+myH2[0].innerHTML = "Yo NINJAS";
+
+document.getElementById("page-title");
+
+//getElementsByTagName() - returns an array of objects
+//getElementsByClassName() - returns an array of objects
+//getElementById() - returns a single object
