f0cfb648a5fbb4edb06d742c81f760588be3418d
leopoldkwok <leopoldkwok@gmail.com>
2016-09-27 15:37:08 +0100

    modules

---
+++ 30_the_net_ninja_angularjs_tutorial/05_Modules/app/app.js
@@ -0,0 +1,14 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+
+// before your application runs
+myNinjaApp.config(function() {
+
+});
+
+// this function fires when the application runs
+myNinjaApp.run(function() {
+
+});
+
+myNinjaApp.controller
