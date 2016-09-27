3a367beacdbe1256439db4a340e1bcf6d1cf9f05
leopoldkwok <leopoldkwok@gmail.com>
2016-09-27 16:33:50 +0100

    controllers

---
+++ 30_the_net_ninja_angularjs_tutorial/06_Controllers/app/app.js
@@ -0,0 +1,9 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+myNinjaApp.controller('NinjaController', ['$scope',function($scope) {
+
+  $scope.message = "hey y'all"
+
+  $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
+
+}]);
