54234415c71fcfc7f2448f0f1a5fae35921517eb
leopoldkwok <leopoldkwok@gmail.com>
2016-09-30 14:12:05 +0100

    ng-include directive

---
+++ 30_the_net_ninja_angularjs_tutorial/08_ng-include-directive/app/app.js
@@ -0,0 +1,28 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+myNinjaApp.controller('NinjaController', ['$scope',function($scope) {
+
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50
+    },
+    {
+      name: "Crystal",
+      belt: "Yellow",
+      rate: 30
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 1000
+    }
+  ];
+
+}]);
