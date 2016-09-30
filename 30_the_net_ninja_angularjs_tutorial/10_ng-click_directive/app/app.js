0bf802c8005628ecc3aa243c73785a4896c4a739
leopoldkwok <leopoldkwok@gmail.com>
2016-09-30 15:45:42 +0100

    ng-click directive

---
+++ 30_the_net_ninja_angularjs_tutorial/10_ng-click_directive/app/app.js
@@ -0,0 +1,38 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+myNinjaApp.controller('NinjaController', ['$scope',function($scope) {
+
+  $scope.removeNinja = function(ninja) {
+
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1)
+  }
+
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50,
+      available: true
+    },
+    {
+      name: "Crystal",
+      belt: "Yellow",
+      rate: 30,
+      available: true
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10,
+      available: false
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 1000,
+      available: true
+    }
+  ];
+
+}]);
