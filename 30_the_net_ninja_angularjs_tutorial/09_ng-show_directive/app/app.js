290c7a237aa34f802e7cbddac2fd659157eeaf63
leopoldkwok <leopoldkwok@gmail.com>
2016-09-30 14:44:46 +0100

    show

---
+++ 30_the_net_ninja_angularjs_tutorial/09_ng-show_directive/app/app.js
@@ -0,0 +1,32 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+myNinjaApp.controller('NinjaController', ['$scope',function($scope) {
+
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50,
+      available: true
+    },
+    {
+      name: "Crystal",
+      belt: "Yellow",
+      rate: 30,
+      available: false
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10,
+      available: false
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 1000,
+      available: true
+    }
+  ];
+
+}]);
