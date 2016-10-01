b318301f3666d23944b2d54fcca1c36b5d0caa1e
leopoldkwok <leopoldkwok@gmail.com>
2016-10-01 09:48:29 +0100

    ng-src_directive

---
+++ 30_the_net_ninja_angularjs_tutorial/12_ng-src_directive/app/app.js
@@ -0,0 +1,56 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+myNinjaApp.controller('NinjaController', ['$scope',function($scope) {
+
+  $scope.removeNinja = function(ninja) {
+
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1)
+
+  }
+
+  $scope.addNinja = function(){
+      $scope.ninjas.push({
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        available: true
+      });
+
+      $scope.newninja.name = "";
+      $scope.newninja.belt = "";
+      $scope.newninja.rate = "";
+  };
+
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50,
+      available: true,
+      thumb: "content/img/yoshi.png"
+    },
+    {
+      name: "Crystal",
+      belt: "Yellow",
+      rate: 30,
+      available: true,
+      thumb: "content/img/crystal.png"
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10,
+      available: true,
+      thumb: "content/img/ryu.png"
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 1000,
+      available: true,
+      thumb: "content/img/shaun.png"
+    }
+  ];
+
+}]);
